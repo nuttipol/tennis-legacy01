695dab87e9ee2f9db9cc3c5f5500d1d928617cad
Nuttipol Chalongmaneerat <nuttipolc@gmail.com>
2021-04-29 12:07:01 +0700

    Tennis game 1st version

---
+++ src/main/java/com/summitthai/tennis/legacy01/Tennis.java
@@ -0,0 +1,118 @@
+package com.summitthai.tennis.legacy01;
+
+public class Tennis {
+
+	private String player1;
+	private String player2;
+	private int player1Point;
+	private int player2Point;
+	private String[] scoreName;
+	
+	public Tennis(String player1,String player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+		this.player1Point = 0;
+		this.player2Point = 0;
+		this.scoreName = new String[] { "love", "fifteen", "thirty", "forty" };
+		System.out.println("Begin Game "+player1+" vs "+player2);
+	}
+
+	public String getPlayer1() {
+		return player1;
+	}
+
+	public void setPlayer1(String player1) {
+		this.player1 = player1;
+	}
+
+	public String getPlayer2() {
+		return player2;
+	}
+
+	public void setPlayer2(String player2) {
+		this.player2 = player2;
+	}
+	
+	public void addPlayer1Point() {
+		this.player1Point+=1;
+		System.out.println(caculateScore());
+	}
+	
+	public void addPlayer2Point() {
+		this.player2Point+=1;
+		System.out.println(caculateScore());
+	}
+	
+	public String caculateScore() {
+		if (player1Point == player2Point) {
+			return deuce(player1Point, player2Point);
+		}else if (player1Point > 3 || player2Point > 3) {
+			return winOrAdvantage(player1Point, player2Point);
+		}else {
+			return ongoing(player1Point, player2Point);
+		}
+	}
+
+	private String deuce(int player1Point,int player2Point) {
+		if (player1Point < 3) {
+			return player1+ " "+ scoreName[player1Point] + " : " + player2 + " "+ scoreName[player2Point]; 
+		}else {
+			return "deuce";
+		}
+	}
+
+	private String winOrAdvantage(int player1Point, int player2Point) {
+		if (player1Point - player2Point >= 2) {
+			return win(player1Point, player2Point);
+		}else{
+			return advantage(player1Point, player2Point);
+		}
+	}
+
+	private String win(int player1Point, int player2Point) {
+		if (player1Point > player2Point) {
+			return "!!! "+ player1+" win game !!!" ;
+		}else {
+			return "!!! "+ player2+" win game !!!";
+		}
+	}
+
+	private String advantage(int player1Point, int player2Point) {
+		if (player1Point > player2Point) {
+			return "player1 advantage";
+		}else {
+			return  "player2 advantage";
+		}
+	}
+
+	private String ongoing(int player1Point, int player2Point) {
+		return player1+ " "+ scoreName[player1Point] + " : " + player2 + " "+ scoreName[player2Point];
+	}
+	
+	public int getPlayer1Point() {
+		return player1Point;
+	}
+
+	public void setPlayer1Point(int player1Point) {
+		this.player1Point = player1Point;
+	}
+
+	public int getPlayer2Point() {
+		return player2Point;
+	}
+
+	public void setPlayer2Point(int player2Point) {
+		this.player2Point = player2Point;
+	}
+
+	public static void main(String[] args) {
+		Tennis t = new Tennis("Federer", "Nadal");
+		t.addPlayer1Point();
+		t.addPlayer1Point();
+		t.addPlayer1Point();
+		t.addPlayer2Point();
+		t.addPlayer1Point();
+	}
+	
+	
+}
